a95a072f2735d49301412150cab3fe536d7119dd
dub-ab <anthonywashington@hotmail.com>
2017-10-09 13:54:40 -0700

    A quotation from "A Wrinkle in Time"

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
+print(" IT WAS A DARK AND STORMY NIGHT.")
+print("")
+print("In her attic bedroom Margaret Murry, wrapped in an old patchwork quilt, sat on the foot of her")
+print("bed and watched the trees tossing in the frenzied lashing of the wind.")
