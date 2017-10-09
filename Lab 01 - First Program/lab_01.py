1b5f591ad094be91ada8ec65dc63f41de8a42fcf
dub-ab <anthonywashington@hotmail.com>
2017-10-09 13:58:50 -0700

    Updated with partial paragraph

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,6 +1,4 @@
 print(" IT WAS A DARK AND STORMY NIGHT.")
 print("")
 print("In her attic bedroom Margaret Murry, wrapped in an old patchwork quilt, sat on the foot of her")
-print("bed and watched the trees tossing in the frenzied lashing of the wind. Behind the trees clouds")
-print("scudded frantically across the sky. Every few moments the moon ripped through them, creating ")
-print("wraithlike shadows that raced along the ground.")
+print("bed and watched the trees tossing in the frenzied lashing of the wind.")
